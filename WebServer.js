a43066560a1cd46829fcdadf11937f74d2c01edb
mimiowo <tomirec2080@gmail.com>
2024-06-04 10:18:02 -0300

    update

--- WebServer.js
+++ WebServer.js
@@ -8,6 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+    console.log('Client connected: ' + req.hostname);
 });
 
 app.listen(PORT, () => {
